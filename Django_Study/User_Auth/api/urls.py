f40aaa74a944d3041813dfb551404218449441a4
ppfuark <phfuark2007@gmail.com>
2025-04-13 19:41:28 -0300

    Django Study | User Auth
    User Authentication

--- Django_Study/User_Auth/api/urls.py
+++ Django_Study/User_Auth/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
-    path('login/', views.LoginView.as_view())
+    path('login/', views.LoginView.as_view()),
+    path('user_auth/', views.UserView.as_view()),
 ]
--- Django_Study/User_Auth/api/views.py
+++ Django_Study/User_Auth/api/views.py
@@ -45,3 +45,20 @@ class LoginView(APIView):
         }
 
         return 
+    
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Unauthenticated!")
+        
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
