851bac472dca4579df2b246368c22b7ef9bcd054
pp.fuark <phfuark2007@gmail.com>
2025-04-15 10:39:55 -0300

    Django Study | CS50 Django
    Tasks APP with session

--- Django_Study/CS50 Django 1/backend/backend/urls.py
+++ Django_Study/CS50 Django 1/backend/backend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('newyear/', include('newyear.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
