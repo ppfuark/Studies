f026b258d27ce4984543c917fa0cfcfb2e468ee4
ppfuark <phfuark2007@gmail.com>
2025-04-24 16:51:45 -0300

    Spring Boot | Store
    Make a Interface for PaymentService

---
+++ Spring Boot/store/src/main/java/com/phfuark/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.phfuark.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- Spring Boot/store/src/main/java/com/phfuark/store/StripePaymentService.java
+++ Spring Boot/store/src/main/java/com/phfuark/store/StripePaymentService.java
@@ -1,6 +1,7 @@
 package com.phfuark.store;
 
-public class StripePaymentService {
+public class StripePaymentService implements PaymentService {
+    @Override
     public void processPayment(double amount){
         System.out.println("STRIPE");
         System.out.printf("Amount: %f", amount);
