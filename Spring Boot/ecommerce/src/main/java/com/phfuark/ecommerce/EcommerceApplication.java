fad0813c3539febcbd870a2dd3e3ec28ef2158d9
ppfuark <phfuark2007@gmail.com>
2025-04-27 21:33:50 -0300

    Spring Boot | Ecommerce
    Created models with relationships (ManyToOne and OneToMany) and configured database settings

--- Spring Boot/ecommerce/src/main/java/com/phfuark/ecommerce/EcommerceApplication.java
+++ Spring Boot/ecommerce/src/main/java/com/phfuark/ecommerce/EcommerceApplication.java
@@ -2,16 +2,12 @@ package com.phfuark.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
-	
 
 }
