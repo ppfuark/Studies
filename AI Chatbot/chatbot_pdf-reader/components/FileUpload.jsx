c20808588c67afd4d60b2aa510e7a0ca06dbdf3c
ppfuark <phfuark2007@gmail.com>
2025-05-06 16:27:53 -0300

    AI Chatbot | Chatbot PDF Reader
    Finished without gemini key

---
+++ AI Chatbot/chatbot_pdf-reader/components/FileUpload.jsx
@@ -0,0 +1,11 @@
+export default function FileUpload(){
+    return(
+        <section>
+            <h2>Get Started</h2>
+            <input 
+            type="file"
+            name="" 
+            id="" />
+        </section>
+    );
+}
---
+++ AI Chatbot/chatbot_pdf-reader/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header>
+            <h1>Nananinanao</h1>
+            <p>Lear from a PDF documents and images</p>
+            <hr />
+        </header>
+    );
+}
