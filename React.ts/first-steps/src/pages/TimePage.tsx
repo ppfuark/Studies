8995f591a82f466e53401d0512af0ea5893ca98e
pp.fuark <phfuark2007@gmail.com>
2025-07-02 10:38:29 -0300

    React.ts | First steps
    Getting current time and sending Good (something) - Finished

--- React.ts/first-steps/src/pages/TimePage.tsx
+++ React.ts/first-steps/src/pages/TimePage.tsx
@@ -3,19 +3,30 @@ import { useEffect, useState } from "react";
 export default function TimePage(){
 
     const [currentTime, setCurrentTime] = useState(new Date);
+    const [hour, SetHour] = useState(new Date().getHours())
+
+    let content = "";
+
+    if (hour >= 0 || hour < 12 ){
+        content = "Good Morning"
+    } else if ( hour >= 12 || hour < 18){
+        content = "Good Afthernoon"
+    }else{
+        content = "Good Night"
+    }
 
     useEffect(()=>{
         const intervalId = setInterval(()=>{
             setCurrentTime(new Date);
+            SetHour(new Date().getHours())
         }, 1000)
         return () => clearInterval(intervalId);
     }, []);
 
-
-
     return(
-        <div>
-            <p>{currentTime.toLocaleTimeString()}</p>
+        <div className="bg-violet-500 flex flex-col h-screen w-screen items-center justify-center">
+            <p className="text-white text-7xl font-bold">{content}</p>
+            <p className="text-white text-5xl font-bold">{currentTime.toLocaleTimeString()}</p>
         </div>
     );
 }
